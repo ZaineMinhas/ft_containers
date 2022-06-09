1b7cf01ad7ef875e3608a26825658a398929422c
ZaineMinhas <zaineminhas5@gmail.com>
2022-06-09 19:21:52 +0200

    eclate/10

--- stack.hpp
+++ stack.hpp
@@ -1,31 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   iterator.hpp                                       :+:      :+:    :+:   */
+/*   stack.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zminhas <zminhas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/07 16:39:36 by zminhas           #+#    #+#             */
-/*   Updated: 2022/06/09 19:13:02 by zminhas          ###   ########.fr       */
+/*   Created: 2022/06/09 19:09:36 by zminhas           #+#    #+#             */
+/*   Updated: 2022/06/09 19:19:04 by zminhas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ITERATOR_HPP
-# define ITERATOR_HPP
+#ifndef STACK_HPP
+# define STACK_HPP
 
-# include <iostream>
-# include <iterator>
-# include <memory>
-# include <deque>
+# include "iterator.hpp"
 
-template <class Category, class T, class Distance = ptrdiff_t, class Pointer = T*, class Reference = T&>
-  struct iterator
-  {
-	typedef T			value_type;
-	typedef Distance	difference_type;
-	typedef Pointer		pointer;
-	typedef Reference	reference;
-	typedef Category	iterator_category;
-  };
+namespace ft
+{
+	template <class T, class Container = std::deque<T> > class stack;
+	class stack {};
+}
 
 #endif
